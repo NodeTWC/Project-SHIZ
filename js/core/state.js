9fdcfce85ba0b91cc79347b0e38ba34ddd07261a
AzahamX <tethurou-y@hotmail.co.jp>
2026-07-06 16:21:43 +0900

    Update state.js

--- js/core/state.js
+++ js/core/state.js
@@ -2,34 +2,31 @@
 
 /*==================================================
 
+    PROJECT SHIZ
+    A.R.C.S.
+
+    Version 3.4.0 Modular Edition
+
     state.js
 
 ==================================================*/
 
-const App = {
 
+const App = {
     state: "READY",
 
     entries: [],
-
     selectedEntry: null,
-
     selectedIndex: -1,
 
     isRunning: false,
 
     settings: {
-
         operatorName: "忠犬しず"
-
     },
 
     config: {
-
         soundEnabled: false,
-
         volume: 0.7
-
     }
-
 };
