71d62b20365dafd797e8a41a3fe35c53cabbb835
AzahamX <tethurou-y@hotmail.co.jp>
2026-07-06 14:47:00 +0900

    Update state.js

--- js/core/state.js
+++ js/core/state.js
@@ -1,25 +1,35 @@
 "use strict";
 
 /*==================================================
-    APP STATE
+
+    state.js
+
 ==================================================*/
 
 const App = {
+
     state: "READY",
 
     entries: [],
+
     selectedEntry: null,
+
     selectedIndex: -1,
 
     isRunning: false,
 
     settings: {
+
         operatorName: "忠犬しず"
+
     },
 
     config: {
-        soundEnabled: false,
-        volume: 0.6
-    }
-};
 
+        soundEnabled: false,
+
+        volume: 0.7
+
+    }
+
+};
