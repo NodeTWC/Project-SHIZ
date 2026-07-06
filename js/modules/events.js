1ad8fff6dcbd3ef24a21a744c13e300b62a9d57d
AzahamX <tethurou-y@hotmail.co.jp>
2026-07-06 16:31:24 +0900

    Update events.js

--- js/modules/events.js
+++ js/modules/events.js
@@ -5,7 +5,7 @@
     PROJECT SHIZ
     A.R.C.S.
 
-    Version 3.4.0 Modular Edition
+    Version 3.4.1 Modular Edition
 
     events.js
 
@@ -16,54 +16,90 @@
     EVENTS
 ==================================================*/
 
-DOM.addEntryButton.addEventListener("click", addEntry);
-
-DOM.clearAllButton.addEventListener("click", clearAllEntries);
-
-DOM.clearLogButton.addEventListener("click", () => {
-    Terminal.clear();
-});
-
-DOM.entryInput.addEventListener("keydown", event => {
-    if (event.key === "Enter") {
-        addEntry();
+function bindEvents() {
+    if (DOM.addEntryButton) {
+        DOM.addEntryButton.addEventListener("click", addEntry);
     }
-});
 
-DOM.startButton.addEventListener("click", () => {
-    Engine.start();
-});
-
-DOM.resetButton.addEventListener("click", resetSystem);
-
-DOM.settingsButton.addEventListener("click", openSettings);
-
-DOM.closeSettingsButton.addEventListener("click", closeSettings);
-
-DOM.saveOperatorButton.addEventListener("click", saveOperatorName);
-
-DOM.operatorInput.addEventListener("keydown", event => {
-    if (event.key === "Enter") {
-        saveOperatorName();
+    if (DOM.clearAllButton) {
+        DOM.clearAllButton.addEventListener("click", clearAllEntries);
     }
-});
 
-DOM.settingsPanel.addEventListener("click", event => {
-    if (event.target === DOM.settingsPanel) {
-        closeSettings();
+    if (DOM.clearLogButton) {
+        DOM.clearLogButton.addEventListener("click", () => {
+            Terminal.clear();
+        });
     }
-});
 
-DOM.openCreatorButton.addEventListener("click", openCreatorPanel);
-
-DOM.closeCreatorButton.addEventListener("click", closeCreatorPanel);
-
-DOM.shareXButton.addEventListener("click", shareToX);
-
-DOM.saveResultButton.addEventListener("click", saveResultPng);
-
-DOM.creatorPanel.addEventListener("click", event => {
-    if (event.target === DOM.creatorPanel) {
-        closeCreatorPanel();
+    if (DOM.entryInput) {
+        DOM.entryInput.addEventListener("keydown", event => {
+            if (event.key === "Enter") {
+                addEntry();
+            }
+        });
     }
-});
+
+    if (DOM.startButton) {
+        DOM.startButton.addEventListener("click", () => {
+            Engine.start();
+        });
+    }
+
+    if (DOM.resetButton) {
+        DOM.resetButton.addEventListener("click", resetSystem);
+    }
+
+    if (DOM.settingsButton) {
+        DOM.settingsButton.addEventListener("click", openSettings);
+    }
+
+    if (DOM.closeSettingsButton) {
+        DOM.closeSettingsButton.addEventListener("click", closeSettings);
+    }
+
+    if (DOM.saveOperatorButton) {
+        DOM.saveOperatorButton.addEventListener("click", saveOperatorName);
+    }
+
+    if (DOM.operatorInput) {
+        DOM.operatorInput.addEventListener("keydown", event => {
+            if (event.key === "Enter") {
+                saveOperatorName();
+            }
+        });
+    }
+
+    if (DOM.settingsPanel) {
+        DOM.settingsPanel.addEventListener("click", event => {
+            if (event.target === DOM.settingsPanel) {
+                closeSettings();
+            }
+        });
+    }
+
+    if (DOM.openCreatorButton) {
+        DOM.openCreatorButton.addEventListener("click", openCreatorPanel);
+    }
+
+    if (DOM.closeCreatorButton) {
+        DOM.closeCreatorButton.addEventListener("click", closeCreatorPanel);
+    }
+
+    if (DOM.shareXButton) {
+        DOM.shareXButton.addEventListener("click", shareToX);
+    }
+
+    if (DOM.saveResultButton) {
+        DOM.saveResultButton.addEventListener("click", saveResultPng);
+    }
+
+    if (DOM.creatorPanel) {
+        DOM.creatorPanel.addEventListener("click", event => {
+            if (event.target === DOM.creatorPanel) {
+                closeCreatorPanel();
+            }
+        });
+    }
+
+    Terminal.system("Event bindings ready.");
+}
